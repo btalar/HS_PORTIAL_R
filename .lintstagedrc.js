e23bd85fde3f8e8cc2c2add7455b74f3880f3afa
bartlomiejtalar <bart-omiej.talar.osv@fedex.com>
2023-10-14 20:42:41 +0200

    test-config

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'src/**/*.{js,jsx,tsx,ts,json}': [
+      'eslint --ext .ts,.tsx --fix src/'
+    ],
+    'framework/src/**/*.{js,jsx,tsx,ts,json}': [
+      'eslint --ext .ts,.tsx --fix src/'
+    ]
+  }
