e2fef8747a172b3c489b90f67d8f72e06d9d252a
bartlomiejtalar <bart-omiej.talar.osv@fedex.com>
2023-10-15 09:56:56 +0200

    pj remove

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const {nextui} = require("@nextui-org/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
