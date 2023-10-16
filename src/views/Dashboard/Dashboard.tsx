46ef9bac740e948aad36d84da26f0f60bb9e19e7
bartlomiejtalar <bart-omiej.talar.osv@fedex.com>
2023-10-16 22:19:50 +0200

    re

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,16 +1,29 @@
 import { Input } from '@nextui-org/react';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
-const appVersion = process.env.HEROKU_RELEASE_VERSION;
-console.log(appVersion, 'HEROKU_RELEASE_VERSION');
-export const Dashboard: FC = () => (
-  <div>
-    <Input
-      type="emails"
-      label="EMAIL"
-      defaultValue="junior@nextui.org"
-      className="max-w-xs"
-    />
-    <h1>Heroku Release Version: {appVersion}</h1>
-  </div>
-);
+export const Dashboard: FC = () => {
+  const [version, setVersion] = useState('');
+
+  useEffect(() => {
+    fetch('/getHerokuReleaseVersion')
+      .then((response) => response.text())
+      .then((data) => {
+        setVersion(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Input
+        type="emails"
+        label="EMAIL"
+        defaultValue="junior@nextui.org"
+        className="max-w-xs"
+      />
+      <h1>Heroku Release Version: {version}</h1>
+    </div>
+  );
+};
