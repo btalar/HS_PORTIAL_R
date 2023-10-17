fd403fa9d0b2231e3b45a0cb4b1d38ee3468480f
bartlomiejtalar <bart-omiej.talar.osv@fedex.com>
2023-10-17 18:43:17 +0200

    deploy

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,9 +1,12 @@
-import { FC } from 'react';
 // import { Route, Routes } from 'react-router-dom';
 //
 // import { Dashboard } from '../Dashboard';
 // import { Login } from '../Login';
+// @ts-ignore
+import metadata from 'heroku-dyno-metadata';
+import { FC } from 'react';
 
+console.log(metadata.dynoName);
 export const App: FC = () => (
   <>sadsd</>
 );
