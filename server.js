e14f5e306c15fe7690631e2b0e4a832d99e2285c
bartlomiejtalar <bart-omiej.talar.osv@fedex.com>
2023-10-17 12:28:27 +0200

    REALASE VERSION

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const dotenv = require('dotenv');
 
 // Load environment variables from .env
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const version = process.env.HEROKU_RELEASE_VERSION;
+
+console.log(version, 'MY API KEY');
 
 // Serve the static React build
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Define your API routes or other server routes as needed
-
-// Handle any other routes by serving the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('https://hotelspot-develop-7cbb71ce24bf.herokuapp.com/getHerokuReleaseVersion', (req, res) => {
+  const herokuReleaseVersion = process.env.HEROKU_RELEASE_VERSION;
+  res.json({ version: herokuReleaseVersion });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,34 +1,18 @@
 import { Input } from '@nextui-org/react';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 
 const apiKey = process.env.API_KEY;
 
 console.log(apiKey);
 
-export const Dashboard: FC = () => {
-  const [version, setVersion] = useState('');
-
-  useEffect(() => {
-    fetch('https://hotelspot-develop-7cbb71ce24bf.herokuapp.com//getHerokuReleaseVersion')
-      .then((response) => response.json())
-      .then((data) => {
-        setVersion(data.version);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <Input
-        type="emails"
-        label="EMAIL"
-        defaultValue="junior@nextui.org"
-        className="max-w-xs"
-      />
-      <h1>Heroku Release Version: {version} {apiKey} </h1>
-    </div>
-  );
-};
+export const Dashboard: FC = () => (
+  <div>
+    <Input
+      type="emails"
+      label="EMAIL"
+      defaultValue="junior@nextui.orgs"
+      className="max-w-xs"
+    />
+    <h1>Heroku Release  {apiKey}</h1>
+  </div>
+);
