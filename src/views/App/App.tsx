3d5427358c109760d1cdc37fea45441a28c73cd1
bartlomiejtalar <bart-omiej.talar.osv@fedex.com>
2023-10-17 20:19:23 +0200

    deploy

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -4,7 +4,7 @@
 // import { Login } from '../Login';
 import { FC } from 'react';
 
-const herokuReleaseVersion = window.HEROKU_RELEASE_VERSION;
+const herokuReleaseVersion = window?.HEROKU_RELEASE_VERSION;
 
 export const App: FC = () => (
   <h1>Heroku Release Version: {herokuReleaseVersion}</h1>
