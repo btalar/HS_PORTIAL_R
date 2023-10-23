19ea45bc374cf9bd1658d1a6ab9e1aa86e3a33aa
bartlomiejtalar <bart-omiej.talar.osv@fedex.com>
2023-10-23 15:17:14 +0200

    Private router

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import { Spinner } from '@nextui-org/react';
+import React, { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 import { auth } from '../../config/firebase';
+import { LoaderWrapper } from './ProtectedRoute.styled';
 
 export const ProtectedRoute: React.FC = () => {
   const navigate = useNavigate();
-
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
+      setLoading(false);
       if (!user) {
         navigate('/login');
       }
@@ -16,5 +19,5 @@ export const ProtectedRoute: React.FC = () => {
     return () => unsubscribe();
   }, [navigate]);
 
-  return <Outlet />;
+  return loading ? <LoaderWrapper><Spinner size="lg" /></LoaderWrapper> : <Outlet />;
 };
