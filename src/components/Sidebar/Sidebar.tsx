cd78e860cf51712e978d185d7eb10547f9acc9bf
bartlomiejtalar <bart-omiej.talar.osv@fedex.com>
2024-01-18 18:20:21 +0100

    HS-SIDEBAR

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,96 +1,107 @@
-import { Accordion, AccordionItem, Divider } from '@nextui-org/react';
+import { Accordion, AccordionItem, Chip, Divider } from '@nextui-org/react';
 import FeatherIcon from 'feather-icons-react';
 import { FC } from 'react';
 
 import { sidebarStore } from '../../store/sidebarStore';
-import { Link } from '../Link';
 import { Logo } from '../Logo';
 import { SidebarElement } from './components/SidebarElement';
-import { LinkList, Sidebar as SidebarWrapper, SidebarInside } from './Sidebar.styled';
+import { Item, LinkList, MenuLink, Sidebar as SidebarWrapper } from './Sidebar.styled';
 
 export const Sidebar:FC = () => {
-  const { isOpen } = sidebarStore();
+  const { isSidebarOpen } = sidebarStore();
 
   return (
-    <SidebarWrapper isOpen={isOpen}>
+    <SidebarWrapper isOpen={isSidebarOpen}>
       <Logo />
       <Divider />
-      <SidebarElement name="/" icon={<FeatherIcon icon="layout" />}>
-        <Link href="/">Dashboard</Link>
+      <SidebarElement name="/" icon={<FeatherIcon size={24} icon="layout" />}>
+        <MenuLink href="/">Dashboard</MenuLink>
       </SidebarElement>
-      <Accordion disabledKeys={['2']}>
-        <AccordionItem key="1" startContent={<FeatherIcon icon="layout" />} title="My Pod's" />
-
+      <Accordion
+        showDivider={false}
+      >
         <AccordionItem
-          key="2"
-          aria-label="Accordion 2"
-          title="Accordion 2"
-          startContent={<FeatherIcon icon="monitor" />}
+          key="1"
+          startContent={<FeatherIcon size={24} icon="monitor" />}
+          title={<Item>Pod</Item>}
         >
           <LinkList>
-            <Link href="/pod">Dane hotelowe</Link>
-            <Link href="/amenities">Udogodnienia</Link>
-            <Link href="/menu">Menu resturacji</Link>
-            <Link href="/settings">Ustawienia</Link>
+            <MenuLink href="/pod">Informacje hotelowe</MenuLink>
+            <MenuLink href="/menu">Menu Restauracja</MenuLink>
+            <MenuLink href="/menu">Oferty specjalne</MenuLink>
+            <MenuLink href="/menu">Reklamy</MenuLink>
+            <MenuLink href="/menu">Web View</MenuLink>
+            <MenuLink href="/settings">Ustawienia</MenuLink>
+          </LinkList>
+        </AccordionItem>
+        <AccordionItem
+          key="2"
+          startContent={<FeatherIcon size={24} icon="award" />}
+          title={<Item>Event Manager <Chip color="danger" size="sm">New</Chip></Item>}
+        >
+          <LinkList>
+            <MenuLink href="/events">Wydarzenia</MenuLink>
+            <MenuLink href="/amenities">Klienci</MenuLink>
+            <MenuLink href="/menu">Sale</MenuLink>
+            <MenuLink href="/settings">Warianty menu</MenuLink>
+            <MenuLink href="/settings">Opiekunowie</MenuLink>
+          </LinkList>
+        </AccordionItem>
+        <AccordionItem key="3" startContent={<FeatherIcon size={24} icon="award" />} title={<Item>Opinie</Item>}>
+          <LinkList>
+            <MenuLink href="/events">Ostatnio Dodane</MenuLink>
+            <MenuLink href="/amenities">Statystyki</MenuLink>
+          </LinkList>
+        </AccordionItem>
+        <AccordionItem
+          key="4"
+          startContent={<FeatherIcon size={24} icon="dollar-sign" />}
+          title={<Item>Prowizje</Item>}
+        >
+          <LinkList>
+            <MenuLink href="/events">Ostatnio Dodane</MenuLink>
+            <MenuLink href="/amenities">Statystyki</MenuLink>
+          </LinkList>
+        </AccordionItem>
+        <AccordionItem
+          key="5"
+          startContent={(
+            <FeatherIcon size={24} icon="alert-triangle" />
+            )}
+          title={<Item>Zgłoszenia <Chip color="danger" size="sm">New</Chip></Item>}
+        >
+          <LinkList>
+            <MenuLink href="/events">Ostatnio Dodane</MenuLink>
+            <MenuLink href="/amenities">Statystyki</MenuLink>
+          </LinkList>
+        </AccordionItem>
+        <AccordionItem
+          key="6"
+          startContent={(
+
+            <FeatherIcon size={24} icon="tool" />
+        )}
+          title={<Item>Usterki <Chip color="danger" size="sm">New</Chip></Item>}
+        >
+          <LinkList>
+            <MenuLink href="/events">Ostatnio Dodane</MenuLink>
+            <MenuLink href="/amenities">Statystyki</MenuLink>
+          </LinkList>
+        </AccordionItem>
+        <AccordionItem key="7" startContent={<FeatherIcon size={24} icon="shopping-cart" />} title={<Item>Store</Item>}>
+          <LinkList>
+            <MenuLink href="/events">Ostatnio Dodane</MenuLink>
+            <MenuLink href="/amenities">Statystyki</MenuLink>
           </LinkList>
         </AccordionItem>
 
       </Accordion>
-
-      <SidebarElement name="/admin" icon={<FeatherIcon icon="shield" />}>
-        <Link href="/admin">Admin</Link>
+      <SidebarElement name="/" icon={<FeatherIcon size={24} icon="users" />}>
+        <MenuLink href="/">Użytkownicy</MenuLink>
       </SidebarElement>
-
-      <SidebarInside>
-
-        <SidebarElement icon={<FeatherIcon icon="monitor" />}>
-          <Accordion className="px-0">
-            <AccordionItem
-              title={<p className="font-14-400">Pod's</p>}
-            >
-              <LinkList>
-                <Link href="/pod">Dane hotelowe</Link>
-                <Link href="/amenities">Udogodnienia</Link>
-                <Link href="/menu">Menu resturacji</Link>
-                <Link href="/settings">Ustawienia</Link>
-              </LinkList>
-            </AccordionItem>
-          </Accordion>
-        </SidebarElement>
-        <SidebarElement icon={<FeatherIcon icon="award" />}>
-          <Accordion className="px-0">
-            <AccordionItem
-              title={<p className="font-14-400">Event Manager</p>}
-            >
-              <LinkList>
-                <Link href="/events">Wydarzenia</Link>
-                <Link href="/amenities">Klienci</Link>
-                <Link href="/menu">Sale</Link>
-                <Link href="/settings">Warianty menu</Link>
-                <Link href="/settings">Opiekunowie</Link>
-              </LinkList>
-            </AccordionItem>
-          </Accordion>
-        </SidebarElement>
-        <SidebarElement name="/" icon={<FeatherIcon icon="message-circle" />}>
-          <Link href="/">Opinie</Link>
-        </SidebarElement>
-        <SidebarElement name="/" icon={<FeatherIcon icon="dollar-sign" />}>
-          <Link href="/">Prowizje</Link>
-        </SidebarElement>
-        <SidebarElement name="/" icon={<FeatherIcon icon="alert-triangle" />}>
-          <Link href="/">Zgłoszenia</Link>
-        </SidebarElement>
-        <SidebarElement name="/" icon={<FeatherIcon icon="tool" />}>
-          <Link href="/">Usterki</Link>
-        </SidebarElement>
-        <SidebarElement name="/" icon={<FeatherIcon icon="shopping-cart" />}>
-          <Link href="/">Store</Link>
-        </SidebarElement>
-        <SidebarElement name="/" icon={<FeatherIcon icon="settings" />}>
-          <Link href="/">Ustawienia</Link>
-        </SidebarElement>
-      </SidebarInside>
+      <SidebarElement name="/" icon={<FeatherIcon size={24} icon="settings" />}>
+        <MenuLink href="/">Ustawienia</MenuLink>
+      </SidebarElement>
     </SidebarWrapper>
   );
 };
